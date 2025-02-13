7a5e9fc020b945c9305756ccdde86b42bfc6f160
Senthilkumar Kannan <125263250+SenthilkumarKannan@users.noreply.github.com>
2025-02-13 09:48:36 +0300

    Update FutureServices.py

--- FutureServices.py
+++ FutureServices.py
@@ -7,12 +7,12 @@ arcpy.env.overwriteOutput ="True"
 arcpy.ClearEnvironment("workspace")
 workspace=arcpy.env.workspace
 tPath=arcpy.env.scratchGDB
-seed=r"Z:\SURVEY DATA\CAD GROUP\3-Support Files\02_Tools\Future_Service_Seed_File.dwt"
+seed=r"C:\Users\skannan\Future_Service_Seed_File.dwt"
 Final_Output =arcpy.GetParameterAsText(0)
 AOI=arcpy.GetParameterAsText(1)
 Dwg_Output=Final_Output + "\Future_Services_" + today + ".DWG"
 
-mxd=arcpy.mapping.MapDocument(r"Z:\SURVEY DATA\CAD GROUP\6-FUTURE SERVICES\Future_Services.mxd")
+mxd=arcpy.mapping.MapDocument(r"C:\Users\skannan\Future_Services.mxd")
 df=arcpy.mapping.ListDataFrames(mxd)[0]
 lay_list=arcpy.mapping.ListLayers(mxd,"",df)
 now=datetime.datetime.now()
